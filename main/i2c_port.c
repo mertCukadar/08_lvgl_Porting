f7d63ea53268a538382b16f84ab1c521d491db8c
mertCukadar <cukadar.mertkaan@gmail.com>
2025-05-12 01:19:46 +0300

    i2c ack runs

--- main/i2c_port.c
+++ main/i2c_port.c
@@ -1,4 +1,8 @@
 #include "i2c_port.h"
+#include "driver/i2c.h"
+#include "esp_log.h"
+
+
 
 int16_t bmp180_read_raw_temperature() {
     uint8_t data[2];
@@ -31,3 +35,25 @@ int16_t bmp180_read_raw_temperature() {
     int16_t raw_temp = (data[0] << 8) | data[1];
     return raw_temp;
 }
+
+
+void mpu6050_check_ack(){
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd); // ← Eksikti
+    i2c_master_write_byte(cmd, (MPU6050_ADDR << 1) | I2C_MASTER_WRITE, true); // ← READ yerine WRITE
+
+    ESP_LOGI("I2C", "MPU6050 ACK check");
+    printf("MPU ADDR: 0x%X\n", (MPU6050_ADDR << 1) | I2C_MASTER_WRITE);
+
+    i2c_master_stop(cmd);
+    esp_err_t ret = i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, pdMS_TO_TICKS(100));
+    i2c_cmd_link_delete(cmd);
+
+    if (ret == ESP_OK) {
+        ESP_LOGI("I2C", "ACK received");
+    } else {
+        ESP_LOGE("I2C", "No ACK received");
+    }
+}
+
+
--- main/i2c_port.h
+++ main/i2c_port.h
@@ -5,9 +5,26 @@
 
 #include "waveshare_rgb_lcd_port.h"
 
-uint16_t bmp180_read_raw_temperature();
+#define I2C_MASTER_SCL_IO 19         // SCL pin
+#define I2C_MASTER_SDA_IO 18         // SDA pin
+#define I2C_MASTER_NUM I2C_NUM_0     // I2C port numarası
+#define I2C_MASTER_FREQ_HZ 100000    // I2C frekansı 100kHz
+#define MPU6050_ADDR 0x68            // MPU6050 I2C adresi
+
+
+#define BMP180_ADDR 0x77
+#define BMP180_REG_CONTROL 0xF4
+#define BMP180_CMD_READ_TEMP 0x2E
+#define BMP180_REG_OUT_MSB 0xF6
+
+#define MPU6050_ADDR 0x68
+
+esp_err_t i2c_master_init();
+
+
+int16_t bmp180_read_raw_temperature();
 
 void mpu6050_read_raw_gyroscope(int16_t *gx, int16_t *gy, int16_t *gz);
 void mpu6050_read_raw_accelerometer(int16_t *ax, int16_t *ay, int16_t *az);
-
+void mpu6050_check_ack();
 #endif // __I2C_PORT_H
