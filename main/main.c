63887753437c04399f4e38bb3c03a2418f5b33d8
Bulut Bekdemir <115174185+bulutbekdemir@users.noreply.github.com>
2025-05-17 01:28:53 +0300

    Update main.c
    Updated comments and delete redefine

--- main/main.c
+++ main/main.c
@@ -4,39 +4,34 @@
 #include <stdio.h>
 #include "ui/ui.h"
 
-#define BMP180_ADDR 0x77
-#define BMP180_REG_CONTROL 0xF4
-#define BMP180_CMD_READ_TEMP 0x2E
-#define BMP180_REG_OUT_MSB 0xF6
-
 lv_label_t *label;
-
-
 
 void app_main()
 {
     waveshare_esp32_s3_rgb_lcd_init();
     lvgl_port_lock(-1);
 
-    // Basit bir label oluştur
+    // Create a basic label
     label = lv_label_create(lv_scr_act());
     lv_label_set_text(label, "Temp: -- °C");
     lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
     lvgl_port_unlock();
 
     while (1) {
+        /* May need to change function name to something like 
+        i2cPort_bmp180_ReadRawTemperature() 
+        because we really do not know where to look for this function. 
+        Look: https://aws.github.io/amazon-freertos/202107.00/html2/main/guide_developer_styleguide.html
+        Section: " Functions (and function-like macros)" */
         int16_t raw_temp = bmp180_read_raw_temperature();
-        float approx_temp = raw_temp / 340.0 + 36.53;  // Yaklaşık değer
+        float approx_temp = raw_temp / 340.0 + 36.53;  //Approx Value
 
         ESP_LOGI("BMP180", "Raw: %d  Temp: %.2f", raw_temp, approx_temp);
 
         //init ui from ui.h
-        // ui_init();
         ui_init();
-        
 
-
-        // Ekranda güncelleme yap
+        // An update on screen
         // if (lvgl_port_lock(-1)) {
         //     char buf[32];
         //     snprintf(buf, sizeof(buf), "Temp: %.2f °C", approx_temp);
