dd7817987cf2dc29be10c46fc8ccd6dc5f839c5a
mertCukadar <cukadar.mertkaan@gmail.com>
2025-05-23 00:25:49 +0300

    basic ui template update for acc , baro , and motor

--- main/main.c
+++ main/main.c
@@ -11,34 +11,17 @@ void app_main()
     waveshare_esp32_s3_rgb_lcd_init();
     lvgl_port_lock(-1);
 
-    // Create a basic label
-    label = lv_label_create(lv_scr_act());
-    lv_label_set_text(label, "Temp: -- °C");
-    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+    ui_init(); // <-- SADECE BURADA BİR KEZ ÇAĞIR!
+
     lvgl_port_unlock();
 
     while (1) {
-        /* May need to change function name to something like 
-        i2cPort_bmp180_ReadRawTemperature() 
-        because we really do not know where to look for this function. 
-        Look: https://aws.github.io/amazon-freertos/202107.00/html2/main/guide_developer_styleguide.html
-        Section: " Functions (and function-like macros)" */
         int16_t raw_temp = bmp180_read_raw_temperature();
-        float approx_temp = raw_temp / 340.0 + 36.53;  //Approx Value
+        float approx_temp = raw_temp / 340.0 + 36.53;
 
         ESP_LOGI("BMP180", "Raw: %d  Temp: %.2f", raw_temp, approx_temp);
-
-        //init ui from ui.h
-        ui_init();
-
-        // An update on screen
-        // if (lvgl_port_lock(-1)) {
-        //     char buf[32];
-        //     snprintf(buf, sizeof(buf), "Temp: %.2f °C", approx_temp);
-        //     lv_label_set_text(label, buf);
-        //     lvgl_port_unlock();
-        // }
 
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
+
